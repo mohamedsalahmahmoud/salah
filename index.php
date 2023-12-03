44ac28175c16327eb227d54b2fe8fa33b508c32a
mohamedsalahmahmoud <87311726+mohamedsalahmahmoud@users.noreply.github.com>
2023-12-03 11:33:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  ob_start();
+  require_once('includes/load.php');
+  if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
+?>
+<?php include_once('layouts/header.php'); ?>
+<div class="login-page">
+    <div class="text-center">
+       <h1>TELECOM EGYPT</h1>
+       <h4>قطاع الاتاحة</h4>
+     </div>
+     <?php echo display_msg($msg); ?>
+      <form method="post" action="auth.php" class="clearfix">
+        <div class="form-group">
+              <label for="username" class="control-label">اسم المستخدم</label>
+              <input type="name" class="form-control" name="username" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <label for="Password" class="control-label">كلمة السر</label>
+            <input type="password" name= "password" class="form-control" placeholder="Password">
+        </div>
+        <div class="form-group">
+                <button type="submit" class="btn btn-danger" style="border-radius:0%">تسجيل الدخول</button>
+        </div>
+    </form>
+</div>
+<?php include_once('layouts/footer.php'); ?>
